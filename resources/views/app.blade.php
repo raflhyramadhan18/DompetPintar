49cf3510fbbe6020bc5a7b74ff62f5d07ec9d40f
Raflhy Nur Ramadhan <raflhy.pragma@gmail.com>
2025-12-18 13:15:25 +0700

    Add logo and pwa files

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,8 +7,8 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <link rel="icon" type="image/png" href="/logo.png">
-<link rel="apple-touch-icon" href="/logo.png">
+        <link rel="icon" type="image/png" href="logo.png">
+<link rel="apple-touch-icon" href="logo.png">
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         
