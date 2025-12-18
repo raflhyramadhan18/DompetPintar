0c80e479e5ebb1928feb06b91d2b9ebd66f52edf
Raflhy Nur Ramadhan <raflhy.pragma@gmail.com>
2025-12-18 12:24:31 +0700

    fix: force https and fix mixed content

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,7 +11,9 @@
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        <link rel="manifest" href="{{ asset('manifest.webmanifest') }}">
+    
+    <meta name="theme-color" content="#16a34a">
         @routes
         @viteReactRefresh
         @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
