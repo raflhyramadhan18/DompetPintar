c623ecacdb4b50c6509d62b0ea584e0f23c63e15
Raflhy Nur Ramadhan <raflhy.pragma@gmail.com>
2025-12-18 12:11:20 +0700

    add deploy

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,8 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <link rel="icon" type="image/svg+xml" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='%2316a34a'><path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M13 7h8m0 0v8m0-8l-8 8-4-4-6 6' /></svg>">
+
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
@@ -16,9 +18,29 @@
         @inertiaHead
 
         <link rel="manifest" href="{{ asset('build/manifest.webmanifest') }}">
+        
+        <style>
+            /* Opsional: Styling sederhana untuk loading screen */
+            #app-loader {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                background-color: #f0fdf4; /* bg-green-100 */
+            }
+            .logo-icon {
+                color: #16a34a; /* text-green-600 */
+                width: 64px;
+                height: 64px;
+            }
+        </style>
     </head>
     <body class="font-sans antialiased">
         @inertia
+
+        <div id="app-loader-container">
+            </div>
+
         <script>
             if ('serviceWorker' in navigator) {
                 window.addEventListener('load', function() {
